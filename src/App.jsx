4cb1c67380d934fdaa66a84e0dbef92935133bcc
olmedo <your.real.email@gmail.com>
2026-09-18 11:44:17 +0800

    Task 2: add /countries/:countryCode route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
+import CountryDetailPage from "./pages/CountryDetailPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -15,7 +16,10 @@ const App = () => {
  <Routes>
  <Route path="/" element={<Layout />}>
  <Route index element={<HomePage />} />
- <Route path="countries" element={<CountriesPage />} />
+ <Route path="countries">
+ <Route index element={<CountriesPage />} />
+ <Route path=":countryCode" element={<CountryDetailPage />} />
+ </Route>
  <Route path="bucket-list" element={<BucketListPage />} />
  <Route path="about" element={<AboutPage />} />
  <Route path="*" element={<NotFoundPage />} />
